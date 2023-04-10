fa0a8197beea3d2725f9f374b4c2c2baad4ca7dc
Innovative Centre IT <innovativecentreit@gmail.com>
2023-04-10 13:21:58 +0500

    added for loop logic to render all items from dict. added feature to use TrackerList dynamically.

--- src/components/TrackerList.jsx
+++ src/components/TrackerList.jsx
@@ -26,24 +26,29 @@ const TrackerList = (props) => {
           </Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="!p-4">
-              <Checkbox />
-            </Table.Cell>
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Complete IELTS 4-5 Student's Book
-            </Table.Cell>
-            <Table.Cell>Unit 1</Table.Cell>
-            <Table.Cell>Yes</Table.Cell>
-            <Table.Cell>
-              <a
-                href="/"
-                className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-              >
-                View
-              </a>
-            </Table.Cell>
-          </Table.Row>
+          {props.levelData.map((item, index) => (
+            <Table.Row
+              key={index}
+              className="bg-white dark:border-gray-700 dark:bg-gray-800"
+            >
+              <Table.Cell className="!p-4">
+                <Checkbox />
+              </Table.Cell>
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {item.book}
+              </Table.Cell>
+              <Table.Cell>{item.unit}</Table.Cell>
+              <Table.Cell>{item.approved}</Table.Cell>
+              <Table.Cell>
+                <a
+                  href="/"
+                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  View
+                </a>
+              </Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
       </Table>
     </div>
