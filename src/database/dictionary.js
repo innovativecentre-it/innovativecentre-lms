0db09f97686bdf5afc191dfdc722153a25eb6e1c
asadbek-gif <asadbekoltinov774@gmail.com>
2023-04-14 07:02:29 +0500

    i have edited the book

--- src/database/dictionary.js
+++ src/database/dictionary.js
@@ -35,94 +35,41 @@ const trackerbase = {
   level2: {
     "Week 1": {
       books: {
-        "IELTS for AP Student's Book ": {
-          units: ["Unit 1", "Unit 2"]
+        "Objective IELTS Advanced": {
+          units: ["Unit 1", "Unit 2", "Unit 3"]
         },
-        "IELTS for AP Booster  ": {
-          units: ["Unit 1", "Unit 2"]
+        "Graduation IELTS": {
+          units: ["Unit 1", "Unit 2", "Unit 3"]
         },
-        " Writing Assignments from Students Book and Workbook ": {
-          units: ["3 Writing Task 2 and 1 Writing Task 1"]
+        "Advantage Reading skills": {
+          units: ["Unit 1", "Unit 2", "Unit 3"]
         },
-        "Listening to Cambridge 7-8 ": {
-          units: ["All"]
+        "Advantage Listening skills": {
+          units: ["Unit 1", "Unit 2", "Unit 3"]
         },
-        "Line charts Assignments": {
-          units: [""]
+        "Advanced Vocabulary for IELTS": {
+          units: ["Unit 1", "Unit 2", "Unit 3"]
         },
-        "Speaking Part 2 Adjectives and descriptions": {
-          units: [""]
-        },
+        "Objective Proficiency": {
+          units: ["Unit 1", "Unit 2", "Unit 3"]
+        }
       }
     },
     "Week 2": {
       books: {
-        "IELTS for AP Students Book ": {
-          units: ["Unit 3", "Unit 4"]
-        },
-        "IELTS for AP Booster ": {
-          units: ["Unit 3", "Unit 4"]
-        },
-        "Writing Assignments from Students Book and Workbook ": {
-          units: ["3 Writing Task 2 and 1 Writing Task 1"]
-        },
-        "Reading Cambridge 7-8 ": {
-          units: ["All"]
-        },
-        "Pie charts Assignments": {
-          units: [""]
-        },
-        "Speaking Part 2 Describing Places ": {
-          units: [""]
-        }
-        
+        // add books for week 2 of level 2 here
       }
     },
     "Week 3": {
       books: {
-        "IELTS for AP Students Book ": {
-          units: ["Unit 5", "Unit 6"]
-        },
-        "IELTS for AP Booster ": {
-          units: ["Unit 5", "Unit 6"]
-        },
-        "Writing Assignments from Students Book and Workbook ": {
-          units: ["3 Writing Task 2 and 1 Writing Task 1"]
-        },
-        "Listening to Cambridge 9-10": {
-          units: ["All"]
-        },
-        "Diagram Assignments": {
-          units: [""]
-        },
-        "Speaking Part 3 Topics  ": {
-          units: [""]
-        }
+        // add books for week 3 of level 2 here
       }
     },
     "Week 4": {
       books: {
-        "IELTS for AP Students Book ": {
-          units: ["Unit 6", "Unit 7"]
-        },
-        "IELTS for AP Booster ": {
-          units: ["Unit 6", "Unit 7"]
-        },
-        "Writing Assignments from Students Book and Workbook ": {
-          units: ["3 Writing Task 2 and 1 Writing Task 1"]
-        },
-        "Reading Cambridge 9-10 ": {
-          units: ["All"]
-        },
-        "Task-2 planning and paragraphing Essays ": {
-          units: [""]
-        },
-        "Speaking Part 1 Topics": {
-          units: [""]
-        }
+        // add books for week 4 of level 2 here
       }
-    },
-    
+    }
   },
   level3: {
     "Week 1": {
@@ -166,44 +113,223 @@ const trackerbase = {
   level4: {
     "Week 1": {
       books: {
-        "Intruduction to IELTS": {
-          units: ["Unit 1", "Unit 2", "Unit 3"]
+        "IELTS for AP Students Book ": {
+          units: ["Unit 1", "Unit 2",]
         },
-        "Graduation skills for IELTS": {
+        "AP Booster Skills": {
           units: ["Unit 1", "Unit 2", "Unit 3"]
+         },
+         "Writing Assignments from Students Book": {
+          units: ["3 Writing task 2", "1 Writing task 1"]
         },
-        "Official Guide to IELTS": {
-          units: ["Unit 1", "Unit 2", "Unit 3"]
+        "Listening to Cambridge 7-8": {
+          units: ["All units"]
         },
-        "New insight into IELTS": {
-          units: ["Unit 1", "Unit 2", "Unit 3"]
-        },
-        "Ready for IELTS": {
-          units: ["Unit 1", "Unit 2", "Unit 3"]
-        },
-        "Mindset for IELTS L1-L3": {
-          units: ["Unit 1", "Unit 2", "Unit 3"]
+        "Line charts Assignments": {
+          units: ["All units"]
+          },
+        "Speaking Part 2 Adjectives and descriptions ": {
+          units: ["All assigments"]
         }
       }
     },
+
     "Week 2": {
       books: {
-        // add books for week 2 of level 3 here
+        "Complete 4-5 Students Book": {
+          units: ["Unit 3", "Unit 4"]
+        },
+        "Complete 4-5 Workbook": {
+         units: ["Unit 3", "Unit 4"]
+        },
+        "True False Not Given Collection": {
+          units: ["All"]
+          },
+          
+          "Speaking Discussion": {
+          units: ["Part 3"]
+          },
+          
+          "Writing Assignments from Students Book and Workbook": {
+          units: ["3 Writing Task 2", "3 Writing Task 1"]
+          }
       }
     },
+
     "Week 3": {
       books: {
-        // add books for week 3 of level 3 here
+        "Complete 4-5 Students Book": {
+          units: ["Unit 5", "Unit 6"]
+          },
+          
+          "Complete 4-5 Workbook": {
+          units: ["Unit 5", "Unit 6"]
+          },
+          
+          "Heading Collection": {
+          units: ["All"]
+          },
+          
+          "Writing Assignments from Students Book and Workbook": {
+          units: ["3 Writing Task 2", "3 Writing Task 1"]
+          },
+          
+          "Speaking Part 1": {
+          units: ["30 topics"]
+          },
+          
+          "Writing Task 2": {
+          units: ["Introduction"]
+          }
       }
     },
+
     "Week 4": {
       books: {
-        // add books for week 3 of level 3 here
+        "Complete IELTS 4-5 SB": {
+          units: ["All"]
+          },
+          
+          "Academic Purposes 5.0-6.5": {
+          units: ["All"]
+          },
+          
+          "Collections for Readings": {
+          units: ["All"]
+          },
+          
+          "Macmillan Reading Skills 4-6": {
+          units: ["All"]
+          },
+          
+          "Macmillan Listening Skills 4-6": {
+          units: ["All"]
+          },
+          
+          "Macmillan Writing Skills 4-6": {
+          units: ["All"]
+          },
+          
+          "Macmillan Reading Skills 6-7.5": {
+          units: ["All"]
+          },
+          
+          "Macmillan Listening Skills 6-7.5": {
+          units: ["All"]
+          },
+          
+          "Macmillan Writing Skills 6-7.5": {
+          units: ["All"]
+          },
+          
+          "Advantage Writing": {
+          units: ["All"]
+          },
+          
+          "Advantage Reading": {
+          units: ["All"]
+          },
+          
+          "Advantages Listening and Speaking": {
+          units: ["All"]
+          },
+          
+          "Mindset for IELTS Foundation": {
+          units: ["All"]
+          },
+          
+          "Mindset for IELTS Level 1": {
+          units: ["All"]
+          },
+          
+          "Mindset for IELTS Level 2": {
+          units: ["All"]
+          },
+          
+          "Mindset for IELTS Level 3": {
+          units: ["All"]
+          },
+          
+          "Vocabulary for IELTS": {
+          units: ["All"]
+          },
+          
+          "Advanced Vocabulary for IELTS": {
+          units: ["All"]
+          },
+          
+          "Introduction to IELTS": {
+          units: ["All"]
+          },
+          
+          "Graduation skills for IELTS": {
+          units: ["All"]
+          },
+          
+          "Official Guide to IELTS": {
+          units: ["All"]
+          },
+          
+          "IELTS Trainer": {
+          units: ["All"]
+          },
+          
+          "Complete 5-6.5": {
+          units: ["All"]
+          },
+          
+          "Complete 6.5-7.5": {
+          units: ["All"]
+          },
+          
+          "Masterclass for IELTS": {
+          units: ["All"]
+          },
+          
+          "Objective IELTS Intermediate": {
+          units: ["All"]
+          },
+          
+          "Objective IELTS Advanced": {
+          units: ["All"]
+          },
+          
+          "New Insight into IELTS": {
+          units: ["All"]
+          },
+          
+          "Cambridge Practices 9-14": {
+          units: ["All"]
+          },
+          
+          "Longman Practices 1-3": {
+          units: ["All"]
+          },
+          
+          "Macmillan Test builder 1-2": {
+          units: ["All"]
+          },
+          
+          "Graduation SB Macmillan": {
+          units: ["All"]
+          },
+          
+          "Macmillan Ready for IELTS": {
+          units: ["All"]
+          },
+          
+          "Exam Essentials": {
+          units: ["All"]
+          },
+          
+          "Cambridge Advanced English": {
+          units: ["All"]
+          }
       }
     },
   },
-}
 
   
 
-export default trackerbase;
+
+}
